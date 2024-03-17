691af29bee92dfec413e66a606fb4e2c05544ec2
MirsalehAghayev <mirsalehaa@code.edu.az>
2024-03-17 15:05:11 +0400

    Practice

---
+++ script.js
@@ -0,0 +1,180 @@
+
+// 1.
+// var eded = prompt("Ədəd daxil edin: ");
+// if (eded % 2 === 0) {
+//     console.log("Ədəd cütdür");
+// } else {
+//     console.log("Ədəd təkdir");
+// }
+
+
+// 2.
+// var a = 10;
+// var b = 5;
+// var c = 15;
+
+// var enBoyuk;
+// if (a > b && a > c)
+// {
+//     enBoyuk = a;
+// } else if (b > a && b > c) 
+// {
+//     enBoyuk = b;
+// } else {
+//     enBoyuk = c;
+// }
+
+
+// 3.
+// var cem = 0;
+// var hasil = 1;
+// for (var i = 1; i <= 10; i++) {
+//     cem += i;
+//     hasil *= i;
+// }
+// console.log("Ədədlərin cəmi: " + cem);
+// console.log("Ədədlərin hasili: " + hasil);
+
+
+
+// 4.
+// var cem = 0;
+// var hasili = 1;
+
+// for (var i = 2; i <= 100; i += 2) {
+//     cem += i;
+//     hasili *= i;
+// }
+
+// console.log("Cüt ədədlərin cəmi: ", cem);
+// console.log("Cüt ədədlərin hasili: ",hasili); 
+
+
+// 5.
+// for (var i = 100; i <= 999; i++)
+// {
+//     if (i % 7 == 0 && i % 8 == 0)
+//     {
+//         console.log(i);
+//     };
+// };
+
+
+
+// 6.
+// var cem = 0;
+// for (var i = 100; i <= 999; i++)
+// {
+//     if (i % 7 == 0 && i % 8 == 0)
+//     {
+//         cem+=i;
+//     };
+// };
+// console.log(cem);
+
+
+// 7.
+// for (var i = 100; i <= 999; i++)
+// {
+//     if (i % 7 == 0 && i % 8 ==0 )
+//     {
+//         console.log(i);
+//         break; 
+//     };
+// };
+
+
+// 9.
+// var eded1 = 123;
+// var eded2 = 456;
+// var eded3 = 789;
+
+// var cem = eded1 + eded2 + eded3;
+// var ortalama = cem / 3;
+
+// console.log("Ədədlərin ortası: " + ortalama);
+
+
+
+// 11.
+// const eded = parseInt(prompt("Reqem daxil edin: "));
+// for (let i = 100; i <= 999; i++)
+// {
+//     if (eded % i == 0 && eded != i)
+//     {
+//         console.log(i);
+//     };
+// };
+
+
+
+// 12.
+// const eded = parseInt(prompt("Reqem daxil edin: "));
+// for (let i = 2; i < eded; i++)
+//  {
+//     if (eded % i == 0)
+//     {
+//         console.log(i);
+//     }
+// };
+
+
+// 14.
+// const eded = 12;
+
+// for (var i = 0; i <= 12; i++)
+// {
+//     for (var j = 0; j <= i; j++)
+//     {
+//         if (i + j == eded)
+//         {
+//             console.log(i, j);
+//         };
+//     };
+// };
+
+
+// 15.
+var x = 5;
+var y = 6;
+
+var te = x;
+x = y;
+y = te;
+
+console.log("x: " + x); 
+console.log("y: " + y); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
